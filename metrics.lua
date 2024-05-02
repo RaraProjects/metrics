232022f7a560b768d38ddfd75219fdd9f40d8a1b
Metra <bryant.delta.mike@gmail.com>
2024-05-01 23:35:55 -0500

    updated version number

--- metrics.lua
+++ metrics.lua
@@ -27,7 +27,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 addon.author = "Metra"
 addon.name = "Metrics"
-addon.version = "0.9.6"
+addon.version = "0.9.7"
 
 _Globals = {}
 _Globals.Initialized = false
