4164ee87f83c6144df7e293cbc3d825b57c17aba
Metra <bryant.delta.mike@gmail.com>
2024-06-02 20:34:24 -0500

    added some guard rails for transparency and scaling settings

--- gui/window/widgets.lua
+++ gui/window/widgets.lua
@@ -22,7 +22,9 @@ end
 Window.Widgets.Alpha = function()
     local alpha = {[1] = Metrics.Window.Alpha}
     UI.SetNextItemWidth(Window.Widgets.Slider_Width)
-    if UI.DragFloat("Window Transparency", alpha, 0.005, 0.1, 1, "%.2f", ImGuiSliderFlags_None) then
+    if UI.DragFloat("Window Transparency", alpha, 0.005, 0.2, 1, "%.2f", ImGuiSliderFlags_None) then
+        if alpha[1] < 0.2 then alpha[1] = 0.2
+        elseif alpha[1] > 1 then alpha[1] = 1 end
         Metrics.Window.Alpha = alpha[1]
     end
     UI.SameLine() Window.Widgets.HelpMarker("Window transparency.")
@@ -34,7 +36,9 @@ end
 Window.Widgets.Window_Scale = function()
     local window_scale = {[1] = Metrics.Window.Window_Scaling}
     UI.SetNextItemWidth(Window.Widgets.Slider_Width)
-    if UI.DragFloat("Window Scaling", window_scale, 0.005, 0.1, 3, "%.2f", ImGuiSliderFlags_None) then
+    if UI.DragFloat("Window Scaling", window_scale, 0.005, 0.7, 3, "%.2f", ImGuiSliderFlags_None) then
+        if window_scale[1] < 0.7 then window_scale[1] = 0.7
+        elseif window_scale[1] > 3 then window_scale[1] = 3 end
         Metrics.Window.Window_Scaling = window_scale[1]
         Window.Scaling_Set = false
         Window.Set_Window_Scale()
