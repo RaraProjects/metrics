d518585fc623ed8e03aaeeb4f3ee89be8f3a6039
Metra <bryant.delta.mike@gmail.com>
2024-08-21 22:22:19 -0500

    added total TP spent to the weaponskill focus menu

--- windows/focus/weaponskills.lua
+++ windows/focus/weaponskills.lua
@@ -15,11 +15,12 @@ Focus.WS.Display = function(player_name, hide_publish)
     local trackable_ws = DB.Enum.Trackable.WS
     local trackable_sc = DB.Enum.Trackable.SC
 
-    if UI.BeginTable("WS and SC", 4, table_flags) then
+    if UI.BeginTable("WS and SC", 5, table_flags) then
         UI.TableSetupColumn("Type", col_flags, name_width)
         UI.TableSetupColumn("Damage", col_flags, width)
         UI.TableSetupColumn("Damage %", col_flags, width)
         UI.TableSetupColumn("Accuracy", col_flags, width)
+        UI.TableSetupColumn("TP Spent", col_flags, width)
         UI.TableHeadersRow()
 
         UI.TableNextRow()
@@ -27,11 +28,13 @@ Focus.WS.Display = function(player_name, hide_publish)
         UI.TableNextColumn() Column.Damage.By_Type(player_name, trackable_ws)
         UI.TableNextColumn() Column.Damage.By_Type(player_name, trackable_ws, true)
         UI.TableNextColumn() Column.Acc.By_Type(player_name, trackable_ws)
+        UI.TableNextColumn() Column.Damage.By_Type_Metric(player_name, trackable_ws, DB.Enum.Metric.TP_SPENT)
 
         UI.TableNextRow()
         UI.TableNextColumn() UI.Text("Skillchains")
         UI.TableNextColumn() Column.Damage.By_Type(player_name, trackable_sc)
         UI.TableNextColumn() Column.Damage.By_Type(player_name, trackable_sc, true)
+        UI.TableNextColumn() UI.TextColored(Res.Colors.Basic.DIM, "---")
         UI.TableNextColumn() UI.TextColored(Res.Colors.Basic.DIM, "---")
         UI.EndTable()
     end
