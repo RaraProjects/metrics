719fd35d45e4a0d0ea91b93e2509a695e0f527e9
Metra <bryant.delta.mike@gmail.com>
2024-09-07 11:10:08 -0500

    added zebra striping to the help text and put the general addon information in a table in the Help window

--- modules/config/_config.lua
+++ modules/config/_config.lua
@@ -98,10 +98,30 @@ end
 -- Shows text commands the user can use.
 ------------------------------------------------------------------------------------------------------
 Config.Section.Text_Commands = function()
-    UI.Text("Bug Reports and ReadMe: https://github.com/RaraProjects/metrics")
-    UI.Text("Version: " .. tostring(addon.version))
+    if UI.BeginTable("Help General", 2, Window_Manager.Table.Flags.Borders) then
+        UI.TableSetupColumn("Col1")
+        UI.TableSetupColumn("Col2")
 
-    UI.Text("Base command: /metrics or /met")
+        UI.TableNextRow()
+        UI.TableNextColumn() UI.Text("GitHub")
+        UI.TableNextColumn() UI.Text("https://github.com/RaraProjects/metrics")
+        Window_Manager.Table_Row_Color(1)
+
+        UI.TableNextColumn() UI.Text("Discord")
+        UI.TableNextColumn() UI.Text("https://discord.gg/u5yqUbR6R7")
+        Window_Manager.Table_Row_Color(0)
+
+        UI.TableNextColumn() UI.Text("Version")
+        UI.TableNextColumn() UI.Text(tostring(addon.version))
+        Window_Manager.Table_Row_Color(1)
+
+        UI.TableNextColumn() UI.Text("Command")
+        UI.TableNextColumn() UI.Text("/metrics or /met")
+        Window_Manager.Table_Row_Color(0)
+
+        UI.EndTable()
+    end
+
     if UI.CollapsingHeader("Commands: General") then
         if UI.BeginTable("General Commands", 3, Window_Manager.Table.Flags.Borders) then
             UI.TableSetupColumn("Full", Column.Flags.None, Config.Full_Width)
@@ -113,18 +133,22 @@ Config.Section.Text_Commands = function()
             UI.TableNextColumn() UI.Text("{none}")
             UI.TableNextColumn()
             UI.TableNextColumn() UI.Text("Toggles settings window.")
+            Window_Manager.Table_Row_Color(1)
 
             UI.TableNextColumn() UI.Text("reset")
             UI.TableNextColumn() UI.Text("r")
             UI.TableNextColumn() UI.Text("Clears the database.")
+            Window_Manager.Table_Row_Color(0)
 
             UI.TableNextColumn() UI.Text("show")
             UI.TableNextColumn() UI.Text("s")
             UI.TableNextColumn() UI.Text("Toggles window visibility.")
+            Window_Manager.Table_Row_Color(1)
 
             UI.TableNextColumn() UI.Text("percent")
             UI.TableNextColumn() UI.Text("")
             UI.TableNextColumn() UI.Text("Toggles showing numerators and denominators for percents.")
+            Window_Manager.Table_Row_Color(0)
 
             UI.EndTable()
         end
@@ -141,18 +165,22 @@ Config.Section.Text_Commands = function()
             UI.TableNextColumn() UI.Text("parse")
             UI.TableNextColumn() UI.Text("")
             UI.TableNextColumn() UI.Text("Switch to the Parse tab.")
+            Window_Manager.Table_Row_Color(1)
 
             UI.TableNextColumn() UI.Text("focus")
             UI.TableNextColumn() UI.Text("")
             UI.TableNextColumn() UI.Text("Switch to the Focus tab.")
+            Window_Manager.Table_Row_Color(0)
 
             UI.TableNextColumn() UI.Text("log")
             UI.TableNextColumn() UI.Text("bl")
             UI.TableNextColumn() UI.Text("Switch to the Battle Log tab.")
+            Window_Manager.Table_Row_Color(1)
 
             UI.TableNextColumn() UI.Text("report")
             UI.TableNextColumn() UI.Text("rep")
             UI.TableNextColumn() UI.Text("Switch to the Report tab.")
+            Window_Manager.Table_Row_Color(0)
 
             UI.EndTable()
         end
@@ -169,30 +197,37 @@ Config.Section.Text_Commands = function()
             UI.TableNextColumn() UI.Text("full")
             UI.TableNextColumn() UI.Text("f")
             UI.TableNextColumn() UI.Text("Shows Parse in full mode.")
+            Window_Manager.Table_Row_Color(1)
 
             UI.TableNextColumn() UI.Text("mini")
             UI.TableNextColumn() UI.Text("m")
             UI.TableNextColumn() UI.Text("Shows Parse in mini mode.")
+            Window_Manager.Table_Row_Color(0)
 
             UI.TableNextColumn() UI.Text("nano")
             UI.TableNextColumn() UI.Text("n")
             UI.TableNextColumn() UI.Text("Shows Parse in nano mode.")
+            Window_Manager.Table_Row_Color(1)
 
             UI.TableNextColumn() UI.Text("pet")
             UI.TableNextColumn() UI.Text("p")
             UI.TableNextColumn() UI.Text("Toggles pet columns in Parse (if not in Focus tab).")
+            Window_Manager.Table_Row_Color(0)
 
             UI.TableNextColumn() UI.Text("dps")
             UI.TableNextColumn() UI.Text("")
             UI.TableNextColumn() UI.Text("Toggles the DPS column.")
+            Window_Manager.Table_Row_Color(1)
 
             UI.TableNextColumn() UI.Text("speed")
             UI.TableNextColumn() UI.Text("")
             UI.TableNextColumn() UI.Text("Toggles the attack speed column.")
+            Window_Manager.Table_Row_Color(0)
 
             UI.TableNextColumn() UI.Text("clock")
             UI.TableNextColumn() UI.Text("c")
             UI.TableNextColumn() UI.Text("Toggles the duration timer visibility.")
+            Window_Manager.Table_Row_Color(1)
 
             UI.EndTable()
         end
@@ -209,34 +244,42 @@ Config.Section.Text_Commands = function()
             UI.TableNextColumn() UI.Text("player {name}")
             UI.TableNextColumn() UI.Text("pl {name}")
             UI.TableNextColumn() UI.Text("Focus on a player in the Focus tab. Partial matching works.")
+            Window_Manager.Table_Row_Color(1)
 
             UI.TableNextColumn() UI.Text("melee")
             UI.TableNextColumn() UI.Text("")
             UI.TableNextColumn() UI.Text("Switch to Melee tab in Focus tab.")
+            Window_Manager.Table_Row_Color(0)
 
             UI.TableNextColumn() UI.Text("ranged")
             UI.TableNextColumn() UI.Text("")
             UI.TableNextColumn() UI.Text("Switch to Ranged tab in Focus tab.")
+            Window_Manager.Table_Row_Color(1)
 
             UI.TableNextColumn() UI.Text("weaponskill")
             UI.TableNextColumn() UI.Text("ws")
             UI.TableNextColumn() UI.Text("Switch to Weaponskill tab in Focus tab.")
+            Window_Manager.Table_Row_Color(0)
 
             UI.TableNextColumn() UI.Text("magic")
             UI.TableNextColumn() UI.Text("")
             UI.TableNextColumn() UI.Text("Switch to Magic tab in Focus tab.")
+            Window_Manager.Table_Row_Color(1)
 
             UI.TableNextColumn() UI.Text("ability")
             UI.TableNextColumn() UI.Text("")
             UI.TableNextColumn() UI.Text("Switch to Ability tab in Focus tab.")
+            Window_Manager.Table_Row_Color(0)
 
             UI.TableNextColumn() UI.Text("pet")
             UI.TableNextColumn() UI.Text("p")
             UI.TableNextColumn() UI.Text("Switch to Pet tab in Focus tab.")
+            Window_Manager.Table_Row_Color(1)
 
             UI.TableNextColumn() UI.Text("defense")
             UI.TableNextColumn() UI.Text("def")
             UI.TableNextColumn() UI.Text("Switch to Defense tab in Focus tab.")
+            Window_Manager.Table_Row_Color(0)
 
             UI.EndTable()
         end
@@ -253,6 +296,7 @@ Config.Section.Text_Commands = function()
             UI.TableNextColumn() UI.Text("xp")
             UI.TableNextColumn() UI.Text("")
             UI.TableNextColumn() UI.Text("Toggles the XP window.")
+            Window_Manager.Table_Row_Color(1)
 
             UI.EndTable()
         end
@@ -269,22 +313,27 @@ Config.Section.Text_Commands = function()
             UI.TableNextColumn() UI.Text("rep total")
             UI.TableNextColumn() UI.Text("")
             UI.TableNextColumn() UI.Text("Publishes total damage report in chat.")
+            Window_Manager.Table_Row_Color(1)
 
             UI.TableNextColumn() UI.Text("rep acc")
             UI.TableNextColumn() UI.Text("")
             UI.TableNextColumn() UI.Text("Publishes accuracy report in chat.")
+            Window_Manager.Table_Row_Color(0)
 
             UI.TableNextColumn() UI.Text("rep melee")
             UI.TableNextColumn() UI.Text("")
             UI.TableNextColumn() UI.Text("Publishes melee report in chat.")
+            Window_Manager.Table_Row_Color(1)
 
             UI.TableNextColumn() UI.Text("rep ws")
             UI.TableNextColumn() UI.Text("")
             UI.TableNextColumn() UI.Text("Publishes weaponskill report in chat.")
+            Window_Manager.Table_Row_Color(0)
 
             UI.TableNextColumn() UI.Text("rep healing")
             UI.TableNextColumn() UI.Text("")
             UI.TableNextColumn() UI.Text("Publishes healing report in chat.")
+            Window_Manager.Table_Row_Color(1)
 
             UI.EndTable()
         end
