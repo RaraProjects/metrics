3ab09e3c609641a7e0431d5ca955dfd8a2e26225
Metra <bryant.delta.mike@gmail.com>
2024-09-07 12:08:09 -0500

    healing received no longer counts towards total damage

--- database/catalog.lua
+++ database/catalog.lua
@@ -302,6 +302,7 @@ end
 ------------------------------------------------------------------------------------------------------
 DB.Catalog.Include_Total_Damage = function(trackable)
 	if trackable == DB.Enum.Trackable.HEALING or
+	   trackable == DB.Enum.Trackable.HEALING_RECEIVED or
 	   trackable == DB.Enum.Trackable.ABILITY_HEALING or
 	   trackable == DB.Enum.Trackable.ABILITY_MP_RECOVERY or
 	   trackable == DB.Enum.Trackable.PET_HEAL or
