1909d5117fa6ac7414acba10465310c0284c8b03
Metra <bryant.delta.mike@gmail.com>
2024-11-05 19:01:35 -0600

    added Aspir to the battle log

--- handlers/spells.lua
+++ handlers/spells.lua
@@ -117,7 +117,7 @@ end
 H.Spell.Blog = function(audits, spell_id, spell_data, spell_name, damage, is_burst, target_count)
     local blog_note = ""
     local space = ""
-    if Res.Spells.Get_Damaging(spell_id) and not Res.Spells.Get_DoT(spell_id) then
+    if (Res.Spells.Get_Damaging(spell_id) or Res.Spells.Get_MP_Drain(spell_id)) and not Res.Spells.Get_DoT(spell_id) then
         -- Show magic burst message.
         if is_burst then
             blog_note = Blog.Enum.Text.MB
