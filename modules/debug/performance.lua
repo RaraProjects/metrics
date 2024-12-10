1294e2f99c724b98c5517c79166ddfcf04f063d0
Metra <bryant.delta.mike@gmail.com>
2024-12-09 22:43:36 -0600

    Updated the performance testing functions and changed the Data Viewer to work with the new database structure.

--- modules/debug/performance.lua
+++ modules/debug/performance.lua
@@ -1,49 +1,18 @@
 Debug.Performance = {}
 
-Debug.Performance.Players = {
-    "Player1",
-    "Player2",
-    "Player3",
-    "Player4",
-    "Player5",
-    "Player6",
-    "Player7",
-    "Player8",
-    "Player9",
-    "Player10",
-    "Player11",
-    "Player12",
-    "Player13",
-    "Player14",
-    "Player15",
-    "Player16",
-    "Player17",
-    "Player18",
-}
-
-Debug.Performance.Mobs = {
-    "Mob1",
-    "Mob2",
-    "Mob3",
-    "Mob4",
-    "Mob5",
-    "Mob6",
-    "Mob7",
-    "Mob8",
-    "Mob9",
-    "Mob10",
-    "Mob11",
-    "Mob12",
-    "Mob13",
-}
+Debug.Performance.Players_To_Add = 18
+Debug.Performance.Mobs_To_Add = 100
 
 ------------------------------------------------------------------------------------------------------
--- Adds a lot of players and mobs that need to be sorted through to test performance.
+-- Adds a lot of players and mobs to test performance under heavy load.
 ------------------------------------------------------------------------------------------------------
 Debug.Performance.Add_Load = function()
-    for _, player in pairs(Debug.Performance.Players) do
-        for _, mob in pairs(Debug.Performance.Mobs) do
-            DB.Data.Update("inc", 25, {player_name = player,  target_name = mob}, "No SC Total", "Total")
+    local player_name, mob_name
+    for x = 1, Debug.Performance.Players_To_Add do
+        for y = 1,  Debug.Performance.Mobs_To_Add do
+            player_name = "Player " .. tostring(x)
+            mob_name = "Mob " .. tostring(y)
+            DB.Data.Update(DB.Update_Mode.INC, 25, {player_name = player_name,  target_name = mob_name}, DB.Trackable.TOTAL_DAMAGE_NO_SKILLCHAIN, DB.Metric.TOTAL)
         end
     end
 end
--- modules/debug/screens/data_viewer.lua
+++ modules/debug/screens/data_viewer.lua
@@ -4,9 +4,42 @@ Debug.Data_View = {}
 -- Populates the data viewer tab.
 ------------------------------------------------------------------------------------------------------
 Debug.Data_View.Populate = function()
-    local stack = T{}
-    if UI.TreeNode("Model.Data") then
+    local stack = {}
+    if UI.TreeNode("DB.Parse") then
         for index, value in pairs(DB.Parse) do
+            if UI.TreeNode(tostring(index)) then
+                Debug.Data_View.Node(stack, value)
+                UI.TreePop()
+            end
+        end
+        UI.TreePop()
+    end
+
+    stack = {}
+    if UI.TreeNode("DB.Parse_Catalog") then
+        for index, value in pairs(DB.Parse_Catalog) do
+            if UI.TreeNode(tostring(index)) then
+                Debug.Data_View.Node(stack, value)
+                UI.TreePop()
+            end
+        end
+        UI.TreePop()
+    end
+
+    stack = {}
+    if UI.TreeNode("DB.Pet_Parse") then
+        for index, value in pairs(DB.Pet_Parse) do
+            if UI.TreeNode(tostring(index)) then
+                Debug.Data_View.Node(stack, value)
+                UI.TreePop()
+            end
+        end
+        UI.TreePop()
+    end
+
+    stack = {}
+    if UI.TreeNode("DB.Pet_Parse_Catalog") then
+        for index, value in pairs(DB.Pet_Parse_Catalog) do
             if UI.TreeNode(tostring(index)) then
                 Debug.Data_View.Node(stack, value)
                 UI.TreePop()
@@ -32,15 +65,15 @@ end
 Debug.Data_View.Node = function(stack, data)
     for index, value in pairs(data) do
         if type(value) == "table" then
-            if index ~= DB.Enum.CATALOG then
-                table.insert(stack, index)
-                Debug.Data_View.Node(stack, value)
-                table.remove(stack)
-            end
+            table.insert(stack, index)
+            Debug.Data_View.Node(stack, value)
+            table.remove(stack)
         else
             if value and value > 0 then
-                if not (index == DB.Metric.MIN and value == DB.Enum.MAX_DAMAGE) then
-                    for _, v in ipairs(stack) do UI.Text(tostring(v)) UI.SameLine() UI.Text(" ") UI.SameLine() end
+                if not (DB.Metric_Needs_Max_Value(index) and value == DB.Enum.MAX_DAMAGE) then
+                    for _, v in ipairs(stack) do
+                        UI.Text(tostring(v)) UI.SameLine() UI.Text("|") UI.SameLine()
+                    end
                     UI.Text(tostring(index) .. ": " .. tostring(value))
                 end
             end
